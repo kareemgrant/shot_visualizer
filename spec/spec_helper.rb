d5677d46f2b87cfecd0d967b5a83b4579ef542ce
Kareem Grant <kareem@getuserwise.com>
2013-06-05 11:04:13 -0600

    Added TravisCI and updated .gitignore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 ENV["RAILS_ENV"] ||= 'test'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec'
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
